72110f063210975caccbf299dcda4499ad5cee94
guibyttoide <guilhermebergamo27@gmail.com>
2024-03-03 20:15:40 -0400

    Add 005 - JavaScript Completo ES6 - Objetos

--- 005 - JavaScript Completo ES6/0206-objetos/script-aula.js
+++ 005 - JavaScript Completo ES6/0206-objetos/script-aula.js
@@ -1,21 +1,73 @@
 var pessoa = {
-	nome: "Andre",
-	idade: 28,
+  nome: "Andre",
+  idade: 28,
 };
 
-console.log(pessoa)
-console.log(pessoa.nome)
-console.log(pessoa.idade)
+console.log(pessoa); // Retorna o Objeto
+console.log(pessoa.idade); // Retorna o valor da propriedade idade dentro do objeto pessoa
+
+// ------------------------------------
 
 var quadrado = {
-	lados: 4,
-	area: function(lado) {
-		return lado * lado;
-	},
-	perimetro: function(lado) {
-		return this.lados * lado;
-	},
-}
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+};
 
-console.log(quadrado.area(5))
-console.log(quadrado.area(4))
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
+
+// ------------------------------------
+
+// Com o ES6+ não é necessário escrever function, reduzindo a versobidade
+var quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+};
+
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
+
+// ------------------------------------
+
+Math.PI();
+Math.random();
+
+var pi = Math.PI();
+var random = Math.random();
+
+console.log(pi);
+console.log(random);
+
+// ------------------------------------
+
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: "#84e",
+};
+
+menu.backgroundColor = "#303030"; // Do Notation Set, Seta um novo valor para a propriedade existente
+menu.color = "#abc"; // Do Notation Get, Porém caso ela não exista, Cria uma nova propriedade com o valor fornecido
+
+// ------------------------------------
+
+var height = 100;
+var menu = {
+  width: 800,
+  height: 50,
+  Metadeheight() {
+    return this.height * 2;
+    return height / 2;
+  },
+  backgroundColor: "#84e",
+};
--- 005 - JavaScript Completo ES6/0206-objetos/script.js
+++ 005 - JavaScript Completo ES6/0206-objetos/script.js
@@ -2,57 +2,51 @@
 // Deve possuir pelo menos duas propriedades nome e sobrenome
 
 var dadosPessoais = {
-  nome: 'Guilherme',
-  sobrenome: 'Bergamo',
+  nome: "Guilherme",
+  sobrenome: "Bergamo",
   idade: 23,
-}
+  // Crie um método no objeto anterior, que mostre o seu nome completo
+  nomeCompleto() {
+    return `${this.nome} ${this.sobrenome}`;
+  },
+};
 
-
-// Crie um método no objeto anterior, que mostre o seu nome completo
-
-dadosPessoais.nomeCompleto = function() {
-    return `${this.nome} ${this.sobrenome}`
-  }
-
-console.log(dadosPessoais.nomeCompleto())
+console.log(dadosPessoais);
+console.log(dadosPessoais.nomeCompleto());
 
 // Modifique o valor da propriedade preco para 3000
 
 var carro = {
   preco: 1000,
   portas: 4,
-  marca: 'Audi',
-}
+  marca: "audi",
+};
 
+console.log(carro.preco);
 carro.preco = 3000;
-console.log(carro.preco)
 
+console.log(carro);
+console.log(carro.preco);
 
 // Crie um objeto de um cachorro que represente um labrador,
 // preto com 10 anos, que late ao ver um homem
 
 var cachorro = {
-  raca: 'labrador',
-  cor: 'preto',
-  idade: '10',
-  viuHomem(late) {
-    if (late === true) {
-      console.log('Latir')
+  raca: "Labrador",
+  cor: "Preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === "homem") {
+      return "latiu ao ver o homem";
     } else {
-      console.log('Não latir')
+      return "não viu ninguem";
     }
-}
-}
+  },
+  cachorroDados() {
+    return `Cachorro da raça ${this.raca} na cor ${this.cor}, com aproximadamente ${this.idade} anos, ele ${this.latir("homem")}`;
+  },
+};
 
-var cachorro = {
-  raca: 'labrador',
-  cor: 'preto',
-  idade: '10',
-  latir() {
-    if (pessoa === 'homem') {
-      return 'Latir'
-    } else {
-      return 'Não latir'
-    }
-}
-}
+// console.log(cachorro);
+
+console.log(cachorro.cachorroDados());
