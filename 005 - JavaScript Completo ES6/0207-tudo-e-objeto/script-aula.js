04bf61d59c20af735263a02733d90027d25a1fe0
guibyttoide <guilhermebergamo27@gmail.com>
2024-03-03 20:45:53 -0400

    Add 005 - JavaScript Completo ES6 - Tudo é Objeto

--- 005 - JavaScript Completo ES6/0207-tudo-e-objeto/script-aula.js
+++ 005 - JavaScript Completo ES6/0207-tudo-e-objeto/script-aula.js
@@ -1,1 +1,15 @@
-var nome = 'André'
+var nome = "Guilherme";
+var nomeMinusculo = nome.toLowerCase();
+var numero = 1.5;
+
+console.log(nome);
+console.log(nomeMinusculo);
+console.log(numero.toFixed());
+
+var btn = document.querySelector(".btn");
+
+btn.classList.add("teste");
+btn.innerHTML; // Retorna o conteúdo do botão
+btn.addEventListener("click", function () {
+  this.classList.toggle("red");
+});
